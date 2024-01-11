2ed081b1f24d952d0a7e21c61803a6e6f2c041f5
AyoAyomide <ayoayomide124@gmail.com>
2024-01-11 20:02:46 +0100

    car controller updated

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,42 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float forwardSpeed;
+    [SerializeField] float acceleration;
     [SerializeField] float turnSpeed;
+    [SerializeField] Transform jetModelGOB;
 
-    [SerializeField] Rigidbody rb;
+    private Rigidbody rb;
 
-    private Vector3 jetDirection;
-    private void Start()
-    {
+    private float curYRot;
 
+    private float horizontalInput;
+    private float verticalInput;
+
+    private bool IS_ACCELERATE;
+    private bool IS_TURN;
+
+    private Vector3 startModelOffset;
+
+    private void Start() {
+
+        rb = gameObject.GetComponent<Rigidbody>();
+        startModelOffset = jetModelGOB.transform.localPosition;
     }
+    
+    private void Update() {
 
-    private void Update()
-    {
+        curYRot += horizontalInput * turnSpeed * Time.deltaTime;
+        //jetModelGOB.position = transform.position + startModelOffset;
+        jetModelGOB.eulerAngles = new Vector3(0,curYRot,0);
 
-        MoveForward();
-
+        HorizontalControl();
         VerticalControl();
     }
 
-    void FixedUpdate()
-    {
-        // Apply force to the rigid body in FixedUpdate
-        rb.AddForce(jetDirection);
+    private void FixedUpdate() {
+
+        if(IS_ACCELERATE){
+            rb.AddForce(jetModelGOB.forward * acceleration * verticalInput, ForceMode.Acceleration);
+        }
+        
     }
 
-    private void MoveForward()
-    {
-        if (jetDirection.z < 30)
-            jetDirection.z += forwardSpeed * Time.deltaTime;
-    }
+    private void HorizontalControl(){
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        IS_TURN = horizontalInput != 0 ? true : false;
 
+        Debug.Log("move left");
+    }
     private void VerticalControl()
     {
         // Get vertical input
-        float verticalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        IS_ACCELERATE = verticalInput != 0 ? true : false;
 
-        jetDirection.x = turnSpeed * verticalInput;
+        Debug.Log("move forward");
     }
 }
